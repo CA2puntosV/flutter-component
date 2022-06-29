0f44c380420da285dd680b29e2f1a77bf1acf69f
cristian <cristian2puntosv@gmail.com>
2022-06-29 17:09:07 -0500

    demostration of json in routes added

--- lib/router/screens.dart
+++ lib/router/screens.dart
@@ -1,3 +1,6 @@
+/* This way of exporting the classes in a single class
+is important to avoid having a lot of imports in each class */
+
 export 'package:components/screens/alert_screen.dart';
 export 'package:components/screens/card_screen.dart';
 export 'package:components/screens/home_screen.dart';
